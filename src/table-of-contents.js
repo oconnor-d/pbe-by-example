92f6d2c3f1a956e5d6106a699aa90ac93ca5aa4c
Daniel O'Connor <oconnor.d@husky.neu.edu>
2020-08-08 23:27:18 -0400

    full jquery; toc scrolling

---
+++ src/table-of-contents.js
@@ -0,0 +1,18 @@
+const SCROLL_OFFSET = 15;
+const SCROLL_DURATION = 1000;
+
+$(() => {
+  const navbarHeight = $('.navbar').height();
+  const goToSection = sectionId => () => {
+    const sectionPosition = $(sectionId).offset().top - navbarHeight - SCROLL_OFFSET;
+    $('html, body').animate({
+      scrollTop: sectionPosition
+    }, SCROLL_DURATION);
+  };
+
+  const tocSections = $('.toc-section, .toc-sub-section');
+  tocSections.each(function() {
+    const tocSection = $(this);
+    tocSection.on('click', goToSection(tocSection.data('target')));
+  });
+});
