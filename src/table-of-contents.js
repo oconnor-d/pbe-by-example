829dcaa3fe67b7b1b4892ce0f18d782e07187a53
Daniel O'Connor <oconnor.d@husky.neu.edu>
2020-08-08 23:33:09 -0400

    stop any scrolling animation when clicking section

--- src/table-of-contents.js
+++ src/table-of-contents.js
@@ -4,6 +4,8 @@ const SCROLL_DURATION = 1000;
 $(() => {
   const navbarHeight = $('.navbar').height();
   const goToSection = sectionId => () => {
+    $('html, body').stop();
+    
     const sectionPosition = $(sectionId).offset().top - navbarHeight - SCROLL_OFFSET;
     $('html, body').animate({
       scrollTop: sectionPosition
