790f01d67f746b56b81ba31279da5a531c4beda5
Daniel O'Connor <oconnor.d@husky.neu.edu>
2020-08-08 23:53:00 -0400

    better toc section scrolling

--- src/table-of-contents.js
+++ src/table-of-contents.js
@@ -2,19 +2,47 @@ const SCROLL_OFFSET = 15;
 const SCROLL_DURATION = 1000;
 
 $(() => {
+  const tocSections = $('.toc-section, .toc-sub-section');
+  let isScrollingToSection = false;
+
+  const setCurrentSection = tocSection => {
+    $(tocSections).removeClass('at-section');
+    $(tocSection).addClass('at-section');
+  }
+
   const navbarHeight = $('.navbar').height();
-  const goToSection = sectionId => () => {
+  const goToSection = tocSection => () => {
     $('html, body').stop();
-    
-    const sectionPosition = $(sectionId).offset().top - navbarHeight - SCROLL_OFFSET;
+
+    isScrollingToSection = true;
+
+    const sectionPosition = $(tocSection.data('target')).offset().top - navbarHeight - SCROLL_OFFSET;
     $('html, body').animate({
       scrollTop: sectionPosition
-    }, SCROLL_DURATION);
+    }, SCROLL_DURATION, () => { isScrollingToSection = false });
+
+    setCurrentSection(tocSection);
   };
 
-  const tocSections = $('.toc-section, .toc-sub-section');
   tocSections.each(function() {
     const tocSection = $(this);
-    tocSection.on('click', goToSection(tocSection.data('target')));
+    tocSection.on('click', goToSection(tocSection));
+  });
+
+  const atOrPastSection = sectionId => {
+    const pageTop = $(window).scrollTop() + navbarHeight;
+    const elementTop = $(sectionId).offset().top;
+
+    return elementTop < pageTop;
+  }
+
+  $(window).scroll(function() {
+    if (isScrollingToSection) return;
+
+    tocSections.each(function () {
+      if (atOrPastSection($(this).data('target'))) {
+        setCurrentSection(this);
+      }
+    })
   });
 });
